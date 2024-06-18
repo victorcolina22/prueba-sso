66d7d5b404ec3289e65a766ac981d0b2fb257958
Victor Colina <victorcolina@G500606MAC272.local>
2024-06-18 09:37:47 -0400

    feat: adding more login options

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
-export const PUBLIC_INDIVIDUAL_POLICY_URL = import.meta.env.PUBLIC_INDIVIDUAL_POLICY_URL
+export const PUBLIC_INDIVIDUAL_POLICY_URL =
+  import.meta.env.PUBLIC_INDIVIDUAL_POLICY_URL ?? "";
+export const SSO_NEXT = import.meta.env.PUBLIC_SSO_NEXT ?? "";
+export const REDIRECT_URI = import.meta.env.PUBLIC_REDIRECT_URI ?? "";
+
