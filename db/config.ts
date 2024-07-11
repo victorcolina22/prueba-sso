be79d0977fa5f71f41bbed5f94802106bb87b5f4
Victor Colina <victorcolina@G500606MAC272.local>
2024-07-11 10:10:19 -0400

    fix: removing token from UI

--- db/config.ts
+++ db/config.ts
@@ -1,12 +1,13 @@
-import { defineDb, defineTable, column } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
 
 const LoginData = defineTable({
   columns: {
     name: column.text(),
-    token: column.text()
-  }
-})
+    token: column.text(),
+    rut: column.text(),
+  },
+});
 
 export default defineDb({
-  tables: { LoginData }
+  tables: { LoginData },
 });
